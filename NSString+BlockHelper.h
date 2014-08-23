4d1384ebdd78f1e5b43d8e7b3cbd12656c9e3cae
bumaociyuan <zx19880528@gmail.com>
2014-08-23 23:23:23 +0800

    zx

---
+++ NSString+BlockHelper.h
@@ -0,0 +1,28 @@
+//
+//  NSString+zxHelper.h
+//  NSStringHelper
+//
+//  Created by zx on 8/23/14.
+//  Copyright (c) 2014 zxstudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (BlockHelper)
+
+/**
+ *  @return self+inputString
+ */
+@property (nonatomic, readonly) NSString *(^add)(NSString *string);
+
+/**
+ *  @return indexOf inputString
+ */
+@property (nonatomic, readonly) NSInteger (^indexOf)(NSString *string);
+
+/**
+ *  @relace 1st param in self with 2nd param
+ */
+@property (nonatomic, readonly) NSString *(^replace)(NSString *targetString,NSString *withString);
+
+@end
