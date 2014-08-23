5695be8d692338ecc028fdac683e9e7ca420ea17
bumaociyuan <zx19880528@gmail.com>
2014-08-23 23:34:42 +0800

    zx

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -19,15 +19,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSString *bumaociyuan = @"bumaociyuan";
-    bumaociyuan = bumaociyuan.add(@" is a boy");
-    NSLog(@"%@",bumaociyuan);
     
-    NSInteger loc = bumaociyuan.indexOf(@"bumaociyuan");
+    NSString *word1 = @"word1";
+    NSString *word2 = @"word2";
+    
+    NSString *word3 = word1.add(@" ").add(word2);
+    NSLog(@"%@",word3);
+    //output : word1 word2
+    
+    NSInteger loc = word3.indexOf(word2);
     NSLog(@"%d",loc);
+    //output : 6
     
-    bumaociyuan = bumaociyuan.replace(@"boy",@"man");
-    NSLog(@"%@",bumaociyuan);
+    NSString *word4 = @"word4";
+    NSString *word5 = @"word5";
+    NSLog(@"%@",word3.replace(word1,word4).replace(word2,word5));
+    //output : word4 word5
 }
 
 - (void)didReceiveMemoryWarning
--- README.md
+++ README.md
@@ -2,3 +2,23 @@ NSString-BlockHelper
 ====================
 
 NSString+BlockHelper
+
+example
+
+	    NSString *word1 = @"word1";
+	    NSString *word2 = @"word2";
+	    
+	    NSString *word3 = word1.add(@" ").add(word2);
+	    NSLog(@"%@",word3);
+	    //output : word1 word2
+	    
+	    NSInteger loc = word3.indexOf(word2);
+	    NSLog(@"%d",loc);
+	    //output : 6
+	    
+	    NSString *word4 = @"word4";
+	    NSString *word5 = @"word5";
+		NSLog(@"%@",word3.replace(word1,word4).replace(word2,word5));
+	    //output : word4 word5
+	    
+**fork and pull request is welcome**
