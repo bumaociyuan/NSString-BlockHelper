deb4d96c141a221d4f3f54ca1eac3df57076112b
zhengxin <zx19880528@qq.com>
2014-11-19 20:36:38 +0800

    zx

--- NSString+BlockHelper/NSString+BlockHelper.m
+++ NSString+BlockHelper/NSString+BlockHelper.m
@@ -1,13 +1,29 @@
 //
-//  NSString+zxHelper.m
-//  NSStringHelper
+//  NSString+BlockHelper.m
+//  NSString+BlockHelper
 //
 //  Created by zx on 8/23/14.
-//  Copyright (c) 2014 zxstudio. All rights reserved.
+//  Copyright (c) 2014 zx. All rights reserved.
 //
 
 #import "NSString+BlockHelper.h"
 
+@interface NSString (Base64)
+
++ (NSString *)_stringWithBase64EncodedString:(NSString *)string;
+- (NSString *)_base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth;
+- (NSString *)_base64EncodedString;
+- (NSString *)_base64DecodedString;
+- (NSData *)_base64DecodedData;
+
+@end
+@interface NSData (Base64)
+
++ (NSData *)_dataWithBase64EncodedString:(NSString *)string;
+- (NSString *)_base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth;
+- (NSString *)_base64EncodedString;
+
+@end
 @implementation NSString (BlockHelper)
 
 - (NSString *(^)())add {
@@ -66,7 +82,7 @@
 }
 
 - (BOOL (^)(NSString *))isContains {
-    BOOL (^ result)(NSString *) = ^ BOOL(NSString *input){
+    BOOL (^ result)(NSString *) = ^BOOL (NSString *input) {
         return self.indexOf(input) != NSNotFound;
     };
     return result;
@@ -129,7 +145,7 @@
 }
 
 - (BOOL (^)())isNumber {
-    BOOL (^result)() = ^() {
+    BOOL (^ result)() = ^() {
         BOOL isValid = NO;
         NSCharacterSet *alphaNumbersSet = [NSCharacterSet decimalDigitCharacterSet];
         NSCharacterSet *stringSet = [NSCharacterSet characterSetWithCharactersInString:self];
@@ -139,9 +155,28 @@
     return result;
 }
 
-//- (BOOL (^)())isIntNumber {
-//    
-//}
+- (BOOL (^)())isIntegerNumber {
+    BOOL (^result)() = ^() {
+        return self.isMatch(@"[1-9][0-9]+");
+    };
+    return result;
+}
+
+- (BOOL (^)())isDecimalNumber {
+    BOOL (^result)() = ^ () {
+        BOOL output = NO;
+        NSArray *splits = [self componentsSeparatedByString:@"."];
+        if (splits.count == 2) {
+            NSString *integer = splits[0];
+            NSString *decimals = splits[1];
+            output = integer.isIntegerNumber() && decimals.isNumber();
+        }else if (splits.count == 1) {
+            output = self.isIntegerNumber();
+        }
+        return output;
+    };
+    return result;
+}
 
 - (BOOL (^)(NSString *))isMatch {
     NSString *originString = self.mutableCopy;
@@ -177,6 +212,70 @@
     return result;
 }
 
+- (NSDictionary *(^)())paramsInUrl {
+    NSDictionary * (^ result)() = ^() {
+        NSMutableDictionary *output = [NSMutableDictionary new];
+        NSArray *params = [self componentsSeparatedByString:@"?"];
+        
+        if (params.count > 1) {
+            NSString *paramsString = params[1];
+            NSArray *components = [paramsString componentsSeparatedByString:@"&"];
+            
+            for (NSString *component in components) {
+                NSInteger index = component.indexOf(@"=");
+                if (index != NSNotFound) {
+                    output[[component substringToIndex:index]] = [component substringFromIndex:index+1];
+                }else {
+                    output[component] = @"";
+                }
+            }
+        }
+        
+        return output;
+    };
+    
+    return result;
+}
+
+- (NSString *(^)(NSString *))paramInUrlWithKey {
+    NSString * (^ result)(NSString *) = ^(NSString *key) {
+        return self.paramsInUrl()[key];
+    };
+    
+    return result;
+}
+
+- (NSString *)urlDecodeUsingEncoding:(NSStringEncoding)encoding {
+    return (__bridge_transfer NSString *)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(NULL,
+                                                                                                 (__bridge CFStringRef)self,
+                                                                                                 CFSTR(""),
+                                                                                                 CFStringConvertNSStringEncodingToEncoding(encoding));
+}
+
+- (NSString *(^)())base64EncodedString {
+    NSString *(^result)() = ^ (){
+        NSString *output = [self _base64EncodedString];
+        return output;
+    };
+    return result;
+}
+
+- (NSString *(^)())base64DecodedString {
+    NSString *(^result)() = ^ (){
+        NSString *output = [self _base64DecodedString];
+        return output;
+    };
+    return result;
+}
+
+- (NSData *(^)())base64DecodedData {
+    NSData *(^result)() = ^ (){
+        NSData *output = [self _base64DecodedData];
+        return output;
+    };
+    return result;
+}
+
 #pragma mark - helper
 - (NSString *)_urlEncode {
     return [self urlEncodeUsingEncoding:NSUTF8StringEncoding];
@@ -194,11 +293,128 @@
     return [self urlDecodeUsingEncoding:NSUTF8StringEncoding];
 }
 
-- (NSString *)urlDecodeUsingEncoding:(NSStringEncoding)encoding {
-    return (__bridge_transfer NSString *)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(NULL,
-                                                                                                 (__bridge CFStringRef)self,
-                                                                                                 CFSTR(""),
-                                                                                                 CFStringConvertNSStringEncodingToEncoding(encoding));
+@end
+
+@implementation NSData (Base64)
+
++ (NSData *)_dataWithBase64EncodedString:(NSString *)string
+{
+    if (![string length]) return nil;
+    
+    NSData *decoded = nil;
+    
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_9 || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_7_0
+    
+    if (![NSData instancesRespondToSelector:@selector(initWithBase64EncodedString:options:)])
+    {
+        decoded = [[self alloc] initWithBase64Encoding:[string stringByReplacingOccurrencesOfString:@"[^A-Za-z0-9+/=]" withString:@"" options:NSRegularExpressionSearch range:NSMakeRange(0, [string length])]];
+    }
+    else
+    
+#endif
+    
+    {
+        decoded = [[self alloc] initWithBase64EncodedString:string options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    }
+    
+    return [decoded length]? decoded: nil;
+}
+
+- (NSString *)_base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth
+{
+    if (![self length]) return nil;
+    
+    NSString *encoded = nil;
+    
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_9 || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_7_0
+    
+    if (![NSData instancesRespondToSelector:@selector(base64EncodedStringWithOptions:)])
+    {
+        encoded = [self base64Encoding];
+    }
+    else
+    
+#endif
+    
+    {
+        switch (wrapWidth)
+        {
+            case 64:
+            {
+                return [self base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+            }
+            case 76:
+            {
+                return [self base64EncodedStringWithOptions:NSDataBase64Encoding76CharacterLineLength];
+            }
+            default:
+            {
+                encoded = [self base64EncodedStringWithOptions:(NSDataBase64EncodingOptions)0];
+            }
+        }
+    }
+    
+    if (!wrapWidth || wrapWidth >= [encoded length])
+    {
+        return encoded;
+    }
+    
+    wrapWidth = (wrapWidth / 4) * 4;
+    NSMutableString *result = [NSMutableString string];
+    for (NSUInteger i = 0; i < [encoded length]; i+= wrapWidth)
+    {
+        if (i + wrapWidth >= [encoded length])
+        {
+            [result appendString:[encoded substringFromIndex:i]];
+            break;
+        }
+        [result appendString:[encoded substringWithRange:NSMakeRange(i, wrapWidth)]];
+        [result appendString:@"\r\n"];
+    }
+    
+    return result;
+}
+
+- (NSString *)_base64EncodedString
+{
+    return [self _base64EncodedStringWithWrapWidth:0];
+}
+
+@end
+
+
+@implementation NSString (Base64)
+
++ (NSString *)_stringWithBase64EncodedString:(NSString *)string
+{
+    NSData *data = [NSData _dataWithBase64EncodedString:string];
+    if (data)
+    {
+        return [[self alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }
+    return nil;
+}
+
+- (NSString *)_base64EncodedStringWithWrapWidth:(NSUInteger)wrapWidth
+{
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    return [data _base64EncodedStringWithWrapWidth:wrapWidth];
+}
+
+- (NSString *)_base64EncodedString
+{
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    return [data _base64EncodedString];
+}
+
+- (NSString *)_base64DecodedString
+{
+    return [NSString _stringWithBase64EncodedString:self];
+}
+
+- (NSData *)_base64DecodedData
+{
+    return [NSData _dataWithBase64EncodedString:self];
 }
 
 @end
